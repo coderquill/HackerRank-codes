7f406a3d8e27b7da116d4ba73e31259d99fb3109
coderquill <coderquill@gmail.com>
2018-12-31 21:39:37 +0530

    Nested lists python

---
+++ py/nestedList.py
@@ -0,0 +1,26 @@
+#Given the names and grades for each student in a Physics class of  students, store them in a nested list and print the name(s) of any student(s) having the second lowest grade.
+
+students = []
+marks=[]
+names=[]
+n=int(input())
+for _ in range(0,n):
+   students.append([raw_input(),float(input())])
+#print students
+for i in range(0,n):
+    marks.append(students[i][1])
+#print marks
+marks.sort()
+#print marks
+#print marks[1]
+j=0
+for i in range (0,n):   
+    if marks[1] == students[i][1]:
+       names.append(students [i][0])
+       j=j+1
+#print names
+#names.sort()
+#print j
+#print names
+for i in range(0,j):
+    print names[i]
